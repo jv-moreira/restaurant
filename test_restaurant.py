50abf1de450b2782a0e6781f208a628ace3e30af
jv-moreira <joaovictorma21@gmail.com>
2022-01-25 13:24:34 -0300

    Creating first tests

--- test_restaurant.py
+++ test_restaurant.py
@@ -1,2 +1,35 @@
 import pytest
 from restaurant import Restaurant
+
+
+def test_pedidos_na_fila_valor_inicial_padrao_igual_a_zero():
+    restaurante = Restaurant("Pizzaria X")
+    assert restaurante.pedidos_na_fila == 0
+
+
+def test_pedidos_na_fila_valor_inicial_maior_que_zero():
+    restaurante = Restaurant("Pizzaria X", 1)
+    assert restaurante.pedidos_na_fila == 1
+
+
+def test_pedidos_na_fila_valor_inicial_menor_que_zero():
+    with pytest.raises(ValueError):
+        Restaurant("Pizzaria X", -1)
+
+
+def test_adiciona_um_pedido_na_fila():
+    restaurante = Restaurant("Pizzaria X", 1)
+    restaurante.adiciona_pedido()
+    assert restaurante.pedidos_na_fila == 2
+
+
+def test_remove_um_pedido_na_fila():
+    restaurante = Restaurant("Pizzaria X", 1)
+    restaurante.remove_pedido()
+    assert restaurante.pedidos_na_fila == 0
+
+
+def test_remove_um_pedido_na_fila_vazia():
+    restaurante = Restaurant("Pizzaria X")
+    restaurante.remove_pedido()
+    assert restaurante.pedidos_na_fila == 0
